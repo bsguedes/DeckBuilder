1cd4a04dc7f9e7a7c432d11dd93627f450ed4f90
Bruno Guedes <bsguedes@inf.ufrgs.br>
2017-08-24 19:22:38 -0300

    [Finish #1] Downloads a list of cards from LigaMagic website

---
+++ scripts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new LigaMagicDownloader();
+            l.Download();
+            Console.ReadLine();            
+        }
+    }
+}

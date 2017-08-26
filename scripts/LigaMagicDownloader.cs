c9bf58db79e1e390614d3c94017498c95a3375bb
Bruno Guedes <bsguedes@inf.ufrgs.br>
2017-08-26 20:00:17 -0300

    organized script usage

--- scripts/LigaMagicDownloader.cs
+++ scripts/LigaMagicDownloader.cs
@@ -9,19 +9,22 @@ using System.Threading.Tasks;
 namespace scripts
 {
     public class LigaMagicDownloader
-    {
-        readonly int MAXIMUM_CARDS = 4500;
-
+    {        
         public Dictionary<int, string> Results = new Dictionary<int, string>();
+        private int start;
+        private int end;
+        private string file;        
 
-        public LigaMagicDownloader()
+        public LigaMagicDownloader(int start, int end, string file)
         {
-
+            this.start = start;
+            this.end = end;
+            this.file = file;
         }
 
         public void Download()
         {
-            for (int i = 4200; i < MAXIMUM_CARDS; i++)
+            for (int i = start; i <= end; i++)
             {
                 RequestData(i, string.Format(@"http://chucktcg.com.br/?view=ecom/item&cardP={0}", i));
             }
@@ -36,7 +39,7 @@ namespace scripts
             string r = data.Substring(start + 13, end - start - 25);
             lock (this.Results)
             {
-                using (StreamWriter sw = new StreamWriter("list.csv", true))
+                using (StreamWriter sw = new StreamWriter(file, true))
                 {
                     sw.WriteLine(string.Format("{0};{1}", index, r));
                 }
--- scripts/Program.cs
+++ scripts/Program.cs
@@ -10,9 +10,23 @@ namespace scripts
     {
         static void Main(string[] args)
         {
-            var l = new LigaMagicDownloader();
-            l.Download();
-            Console.ReadLine();            
+            if (args[0] == "-h")
+            {
+                Console.WriteLine("List of commands:");
+                Console.WriteLine("-l start end file     Downloads all index between start and end from LigaMagic and stores it in file.");
+            }
+            else if (args[0] == "-l" && args.Length >= 4)
+            {
+                int start = int.Parse(args[1]);
+                int end = int.Parse(args[2]);
+                string file = args[3];
+                var l = new LigaMagicDownloader(start, end, file);
+                l.Download();
+            }
+            else
+            {
+                Console.WriteLine("Invalid command, type -h for help.");
+            }
         }
     }
 }
